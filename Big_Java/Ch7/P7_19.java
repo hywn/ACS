1e163a4e2aa23cf48346ca256b1b0e16edab3c2b
abcjcce <seawee65@live.com>
2018-11-13 15:58:25 -0500

    grafisch

---
+++ Big_Java/Ch7/P7_19.java
@@ -0,0 +1,79 @@
+public class P7_19 {
+
+	static final String
+		FULL = "@@",
+		EMPTY = "  ",
+		HALF_BOT = "..",
+		HALF_TOP = "''",
+		ERROR = "??";
+
+	interface Function {
+		double eval (double x);
+	}
+
+	public static void main (String[] args) {
+
+		Function f = x -> 2.4 * Math.sin (x); // this is the book's function
+
+		/* this program can graph other functions. here are some examples: */
+		//f = x -> 1 / x;
+		//f = x -> Math.pow (x, 2) / (x - 6);
+		f = x -> Math.sqrt (x);
+		//f = x -> { if (x < 1) return x; else return 0.1 * Math.pow (Math.E, x); };
+
+		int plotSizeX = 100; // plot size in terms of character things, not the actual numeric value
+		int plotSizeY = 50;
+		double scale = 0.1; // how much one character thing is
+
+		graph (f, scale, plotSizeX, plotSizeY);
+
+	}
+
+	// scale is width per pixel
+	public static void graph (Function f, double scale, int width, int height) {
+
+		int halfHeight = height / 2;
+		int halfWidth = width / 2;
+
+		for (int h = halfHeight; h > -halfHeight; h--) {
+
+			System.out.println (getRow (f, scale, -halfWidth * scale, halfWidth * scale, h * scale));
+
+		}
+	}
+
+	// graphs function from x=a to x=b and y=a to y=b
+	public static String getRow (Function f, double scale, double a, double b, double c) {
+
+		StringBuilder row = new StringBuilder ();
+
+		double halfScale = scale / 2;
+
+		for (double x = a; x < b; x += scale) {
+
+			double y;
+
+			try {
+
+				y = f.eval (x - halfScale);
+
+			}
+
+			catch (ArithmeticException e) { row.append (ERROR); continue; }
+
+			// not abs
+			double dif = (y > 0) ? y - c : c - y;
+
+			// if the real value is closer to zero AND it doesn't go over
+			if (dif >= halfScale && dif <= Math.abs (y)) row.append (FULL);
+				//else row.append (' ');
+			else if (dif <= 0 || dif >= Math.abs (y)) row.append (EMPTY);
+			else row.append ((y > 0) ? HALF_BOT : HALF_TOP);
+
+		}
+
+		return row.toString ();
+
+	}
+
+}
--- Big_Java/Ch7/Testing/Test1.java
+++ Big_Java/Ch7/Testing/Test1.java
@@ -1,11 +1,13 @@
+package Testing;
+
 public class Test1 {
 
 	public static void main (String[] args) {
-	
+
 		String[] yesNo = { "Test" };
-		
+
 		System.out.println (yesNo.length);
-	
+
 	}
 
 }
