4bbe67907de6c9a8da0a7597be3788118fa52b62
abcjcce <seawee65@live.com>
2018-11-27 09:52:41 -0500

    finished tic tac toe

--- Big_Java/Ch7/P7_8.java
+++ Big_Java/Ch7/P7_8.java
@@ -2,74 +2,68 @@ import java.util.Scanner;
 
 public class P7_8 {
 
-	// TODO: cannot mark over other player
-	// TODO: coord validation
-
-	public static char BLANK = '-';
-	public static int SIZE = 3;
-	public static char[] AXIS_X = { 'A', 'B', 'C', 'D', 'E', 'F', 'G' },
-		AXIS_Y = { '1', '2', '3', '4', '5', '6', '7' };
+	public static char BLANK = '-', NULL = ' ';
+	public static int SIZE = 3; // can make bigger up to 9
+	public static char[] AXIS_X = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I' }, AXIS_Y = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
 	public static void main (String[] args) {
 
 		char[][] board = new char[SIZE][SIZE];
+		for (int i = 0; i < SIZE; i++) for (int j = 0; j < SIZE; j++) board[i][j] = BLANK; // fill board with blanks
 
-		for (int i = 0; i < SIZE; i++) for (int j = 0; j < SIZE; j++) board[i][j] = '-';
-
-		final char[] PLAYERS = { 'X', 'O' };
+		final char[] PLAYERS = { 'X', 'O' }; // can add any number more players
 		int turn = 0;
-
 		char winner;
 
 		Scanner scan = new Scanner (System.in);
 
-		while ((winner = getWinner (board)) == ' ') {
+		while ((winner = getWinner (board)) == NULL) {
 
 			printBoard (board);
 
 			System.out.printf ("Player %c, please enter your mark (e.g. B2): ", PLAYERS[turn]);
 
-			String coord = scan.next ();
+			if (!set (board, scan.next ().trim (), PLAYERS[turn])) {
 
-			coord = coord.trim ();
+				System.out.println ("You can't move there!");
+				continue;
 
-			if (coord.length () != 2) { System.out.println ("Improper input, try again."); continue; }
-
-			set (board, coord, PLAYERS[turn]);
+			}
 
 			turn = (turn + 1) % PLAYERS.length;
 
 		}
 
 		System.out.printf ("Player %c has won.%n", winner);
-
 		printBoard (board);
 
+	}
+
+	// returns whether it set correctly
+	static boolean set (char[][] board, String coord, char toSet) {
+
+		try {
+
+			if (coord.length () != 2) return false;
+
+			char[] coords = coord.toCharArray ();
+
+			int x = indexOf (AXIS_X, coords[0]);
+			int y = indexOf (AXIS_Y, coords[1]);
+
+			if (board[y][x] != BLANK) return false;
+
+			board[y][x] = toSet;
+
+			return true;
+
+		}
+
+		catch (ArrayIndexOutOfBoundsException e) { return false; }
 
 	}
 
-	// a series of SIZE numbers
-	static interface Series {
-		char get (int n, int i, char[][] board);
-	}
-
-	static Series[] tests = {
-		(n, i, board) -> (n == 0) ? board[i][i] : board[SIZE - 1 - i][i], // diagonals, will repeat 2 or 3 extra times but oh well
-		(n, i, board) -> board[i][n], // horizontals
-		(n, i, board) -> board[n][i] // verticals
-	};
-
-	public static void set (char[][] board, String coord, char toSet) {
-
-		char[] coords = coord.toCharArray ();
-
-		int x = indexOf (AXIS_X, coords[0]), y = indexOf (AXIS_Y, coords[1]);
-
-		board[y][x] = toSet;
-
-	}
-
-	public static int indexOf (char[] array, char val) {
+	static int indexOf (char[] array, char val) {
 
 		val = Character.toUpperCase (val);
 
@@ -79,25 +73,21 @@ public class P7_8 {
 
 	}
 
-	public static void printBoard (char[][] board) {
+	static final int PADDING = 2;
 
-		StringBuilder b = new StringBuilder ();
+	static void printBoard (char[][] board) {
 
-		b.append (' ');
+		StringBuilder b = new StringBuilder ("\n "); pad (b, PADDING);
 
-		for (int i = 0; i < SIZE; i++) b.append (AXIS_X[i]);
+		for (int i = 0; i < SIZE; i++) { b.append (AXIS_X[i]); pad (b, PADDING); } // print horiz. axis
 
 		b.append ('\n');
 
 		for (int i = 0; i < SIZE; i++) {
 
-			b.append (AXIS_Y[i]);
+			b.append (AXIS_Y[i]); pad (b, PADDING); // print vert. axis
 
-			for (int j = 0; j < SIZE; j++) {
-
-				b.append (board[i][j]);
-
-			}
+			for (int j = 0; j < SIZE; j++) { b.append (board[i][j]); pad (b, PADDING); } // print each board space
 
 			b.append ('\n');
 
@@ -107,7 +97,20 @@ public class P7_8 {
 
 	}
 
-	public static char getWinner (char[][] board) {
+	static void pad (StringBuilder b, int num) { for (int i = 0; i < num; i++) b.append (' '); }
+
+	// a series of SIZE numbers
+	static interface Series {
+		char get (int n, int i, char[][] board);
+	}
+
+	static Series[] tests = { // should work with any-sized board
+		(n, i, board) -> (n == 0) ? board[i][i] : board[SIZE - 1 - i][i], // diagonals, will repeat 2 or 3 extra times but oh well
+		(n, i, board) -> board[i][n], // horizontals
+		(n, i, board) -> board[n][i] // verticals
+	};
+
+	static char getWinner (char[][] board) {
 
 		for (Series test : tests) {
 
@@ -128,15 +131,13 @@ public class P7_8 {
 
 				}
 
-				System.out.println (first);
-
 				if (!lost) return first;
 
 			}
 
 		}
 
-		return ' ';
+		return NULL;
 
 	}
 
