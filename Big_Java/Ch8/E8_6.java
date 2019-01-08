9ccebc14044375043e78be0225417212ac0f6564
abcjcce <seawee65@live.com>
2019-01-08 10:28:47 -0500

    ready for ch8

---
+++ Big_Java/Ch8/E8_6.java
@@ -0,0 +1,23 @@
+public class E8_6 {
+
+	static class Geometry {
+
+		public static double cubeVolume (double h) { return Math.pow (h, 3); }
+
+		public static double cubeSurface (double h) { return 6 * h; }
+
+		public static double sphereVolume (double r) { return (4.0 / 3.0) * Math.PI * Math.pow (r, 2); }
+
+		public static double sphereSurface (double r) {}
+
+		public static double cylinderVolume (double r, double h) {}
+
+		public static double cylinderSurface (double r, double h) {}
+
+		public static double coneVolume (double r, double h) {}
+
+		public static double coneSurface (double r, double h) {}
+
+	}
+
+}
---
+++ Big_Java/Ch8/P8_1.java
@@ -0,0 +1,2 @@
+public class P8_1 {
+}
