cd594baf9ca414782105e444d417d49a064b17a2
abcjcce <seawee65@live.com>
2019-01-10 10:47:05 -0500

    Update ComboLock.java

--- Big_Java/Ch8/P8_1/ComboLock.java
+++ Big_Java/Ch8/P8_1/ComboLock.java
@@ -1,11 +1,17 @@
 public class ComboLock {
 
+	private int currOrder;
 	private int[] secret;
+	private boolean[] unlocked;
 
-	public ComboLock (int... secret) { this.secret = secret; }
+	public ComboLock (int... secret) {
+		this.secret = secret;
+		unlocked = new boolean[secret.length];
+		currOrder = 0;
+	}
 	public void reset () {}
 	public void turnLeft (int ticks) {}
 	public void turnRight (int ticks) {}
 	public boolean open () {}
-	
+
 }
